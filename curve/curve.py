0f22e4a3feed1c84dd58d4606dc48c7b90d68322
Colin Hartley <colin.hartley@opendatadsl.com>
2026-03-03 14:23:40 +0000

    curve

--- curve/curve.py
+++ curve/curve.py
@@ -86,7 +86,7 @@ async def run(task):
 			await odsl_process.startPhase("BUILD")
 			await odsl_process.logMessage("Building " + id + ":" + name)
 			obj = {'_id': id}
-			obj['name'] = timespread(base_curve)
+			obj['name'] = timespread(base_curve).data
 			odsl.update('object', 'private', obj)
 			await odsl_process.endPhase("success", "Updating Successfully")
 			await odsl_process.endProcess("success", "Completed Successfully")
