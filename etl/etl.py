d9a228e88673055acf765fe807a654f9f2e311cf
Colin Hartley <colin.hartley@opendatadsl.com>
2026-03-04 12:26:13 +0000

    Changes

--- etl/etl.py
+++ etl/etl.py
@@ -1,14 +1,15 @@
 import asyncio
 import os
 import sys
+import json
 from odsl import process
 from odsl import sdk
 import random
 import datetime
+from datetime import date
 
 async def run(task):
 	odsl = sdk.ODSL()
-	odsl.setStage(os.getenv('ODSL_STAGE'))
 	user='alex.lynch@glencore.co.uk'
 	apikey='69a813b0d6152d543360a8ed'
 	odsl.loginWithAPIKey(user, apikey)
@@ -26,41 +27,48 @@ async def run(task):
 			await odsl_process.startProcess()
 
 			# Create the object to update
+			print("starting phase LOAD")
 			await odsl_process.startPhase("LOAD")
+			dnow = date.now() - datetime.timedelta(days=1)
+			ondate = dnow.isoformat()
 			obj = {
 				'_id': 'AAA.PYTHON',
 				'EVENTS': [
 					{
-						'_id': 'DEMO-2026-03-02-M01',
+						'_id': 'DEMO-'+ondate+'-M01',
 						'_type': 'VarEvent',
 						'_dsid':'ODSL.PYTHON.TRADER',
 						'eventtype': 'TraderPrices',
-						'eventtime': '2026-03-02',
+						'eventtime': ondate,
 						'tenor': 'M01',
 						'price': random.randrange(1, 100)
 					},
 					{
-						'_id': 'DEMO-2026-03-02-M02',
+						'_id': 'DEMO-'+ondate+'-M02',
 						'_type': 'VarEvent',
 						'_dsid':'ODSL.PYTHON.TRADER',
 						'eventtype': 'TraderPrices',
-						'eventtime': '2026-03-02',
+						'eventtime': ondate,
 						'tenor': 'M02',
 						'price': random.randrange(1, 100)
 					},
 					{
-						'_id': 'DEMO-2026-03-02-M03',
+						'_id': 'DEMO-'+ondate+'-M03',
 						'_type': 'VarEvent',
 						'_dsid':'ODSL.PYTHON.TRADER',
 						'eventtype': 'TraderPrices',
-						'eventtime': '2026-03-02',
+						'eventtime': ondate,
 						'tenor': 'M03',
 						'price': random.randrange(1, 100)
 					}
 				]
 			}
+			print("Logging message")
 			await odsl_process.logMessage(datetime.datetime.now().isoformat() + " info Updating " + obj['_id'])
+			print("Updating object")
+			print(json.JSONEncoder().encode(o=obj))
 			odsl.update('object', 'private', obj, {'_origin':t['name']})
+			print("Ending phase LOAD")
 			await odsl_process.endPhase("success", "Updating Successfully")
 			await odsl_process.endProcess("success", "Completed Successfully")
 
