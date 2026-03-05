eb18d37846662e2fe7b166850780d804db6d45b0
Colin Hartley <colin.hartley@opendatadsl.com>
2026-03-05 12:24:26 +0000

    Changes

---
+++ etl/etl-email.py
@@ -0,0 +1,73 @@
+import asyncio
+import os
+import sys
+import json
+from odsl import process
+from odsl import sdk
+import random
+import datetime
+from datetime import date
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def addEvent(ondate, tenor, value):
+    return {
+			'_id': 'DEMO-'+ondate+'-'+tenor,
+			'_type': 'VarEvent',
+			'_dsid':'ODSL.PYTHON.TRADER',
+			'eventtype': 'TraderPrices',
+			'eventtime': ondate,
+			'relative': tenor,
+			'price': value
+		}
+
+async def run(task):
+	odsl = sdk.ODSL()
+	user=os.getenv('user')
+	apikey=os.getenv('apikey')
+	odsl.loginWithAPIKey(user, apikey)
+    # Get the task details
+	t = odsl.get('process-task', None, task)
+	print(t)
+	if t is not None:
+		# Get the process details		
+		p = odsl.get('process', None, t['name'])
+		if p is not None:
+			print(p)
+   
+			# Create the process message
+			odsl_process = process.TASK(p, t)
+			await odsl_process.startProcess()
+			try:
+				# Create the object to update
+				print("starting phase LOAD")
+				await odsl_process.startPhase("LOAD")
+				dnow = date.today() - datetime.timedelta(days=1)
+				ondate = dnow.isoformat()
+				input = t['input']
+				ondate = input['ondate']
+				await odsl_process.logMessage(datetime.datetime.now().isoformat() + " info Running ETL for " + ondate)
+				obj = {
+					'_id': 'AAA.PYTHON',
+					'EMAILEVENTS': []
+				}
+				data = int(input['data'])
+				for x in data:
+					obj['EMAILEVENTS'].append(addEvent(ondate, data.tenor, data.value))
+				print("Logging message")
+				await odsl_process.logMessage(datetime.datetime.now().isoformat() + " info Updating " + obj['_id'])
+				print("Updating object")
+				print(json.JSONEncoder().encode(o=obj))
+				odsl.update('object', 'private', obj, {'_origin':t['name']})
+				print("Ending phase LOAD")
+				await odsl_process.endPhase("success", "Updating Successfully")
+				await odsl_process.endProcess("success", "Completed Successfully")
+			except Exception as e:
+				await odsl_process.endProcess("failed", repr(e))
+
+task = sys.argv[1]
+print("Running Process Task: " + task)
+asyncio.run(run(task))
+print("Finished Process Task")
+print("-----------------------")
